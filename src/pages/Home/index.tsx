58398ad37b4eb8f6ec33558bd9058d58772a5d82
ropoko <rodrigostramantinoli@gmail.com>
2022-10-14 19:56:55 -0300

    feat: credits page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,6 +17,45 @@ function Home() {
 					<ButtonLink link="/tips" color="a6131a" text="Dicas" />
 				</div>
 			</div>
+			<div className="credits">
+				<h1>Sobre o projeto</h1>
+
+				<h2>Objetivo</h2>
+				<p className="objective-desc">
+					O REGER é uma iniciativa da Diretora da Área de Ciências Exatas,
+					Humanas e Sociais, Prof. Dra. Beatriz Antoniassi e da Dra. Dulce
+					Helena Jardim Constantino, e também tem o propósito de chamar a
+					atenção para que cada ser humano possa fazer a sua parte e ajudar a
+					proteger e respeitar o meio ambiente, garantindo um futuro melhor para
+					as novas gerações.
+				</p>
+
+				<div className="authors-dev">
+					<h3>Desenvolvedores</h3>
+					<p>
+						<strong>Professor: </strong>Vinicius Santos Andrade - Ciência da
+						Computação
+					</p>
+					<ul>
+						<li>Rodrigo Maganha Stramantinoli</li>
+						<li>Roger Santos Barreto</li>
+						<li>Felipe Evaristo dos Santos</li>
+						<li>Leandro Gomes Zavan</li>
+					</ul>
+				</div>
+
+				<div className="authors-design">
+					<h3>Designers</h3>
+					<p>
+						<strong>Professora: </strong>Sileide Aparecida - Design
+					</p>
+					<ul>
+						<li>Amanda Scola Gaioti</li>
+						<li>Davi Ramos</li>
+						<li>Hanna Govedice</li>
+					</ul>
+				</div>
+			</div>
 		</>
 	)
 }
