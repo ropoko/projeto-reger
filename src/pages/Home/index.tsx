03f1734fec83277feef28b910956411bb62bc2af
Roger Santos Barreto <roger.barreto@institutoeducar.com.br>
2022-11-11 17:32:00 -0300

    feat: home responsiva

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,8 +18,8 @@ function Home() {
 				<div className="buttons">
 					<ButtonLink link="/project" color="fc000a" text="O Projeto" />
 					<ButtonLink link="/about" color="a6131a" text="O quê fazemos" />
-					<ButtonLink link="/contact" color="fc000a" text="Onde estamos" />
-					<ButtonLink link="/tips" color="a6131a" text="Dicas" />
+					<ButtonLink link="/contact" color="cd151c" text="Onde estamos" />
+					<ButtonLink link="/tips" color="850d12" text="Dicas" />
 				</div>
 			</div>
 			<Team />
